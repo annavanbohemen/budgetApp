0514a13f141d7b6c7e68fd7884a674cb2bc11784
Anna van Bohemen <anna@vanbohemen.com>
2018-07-12 10:59:58 +0200

    setting up eventlistener

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var budgetController = (function() {
 
-    var x = 23;
+ // some code
 
-    var add = function(a) {
-        return x + a;
-    }
 })();
 
 var UIController = (function() {
@@ -14,6 +11,35 @@ var UIController = (function() {
 })();
 
 var appController = (function(budgetCtrl, UICtrl) {
+
+    var addItem = function() {
+
+        // 1. input data
+
+        // 2. Add item to budgetController
+
+        //3. Add item to UIController
+
+        //4. calculate the budget
+
+        //5. display the budget - UI
+        console.log('it works')
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        
+        addItem();
+
+    });
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            addItem();
+        }
+       
+    });
+
 
 
 })(budgetController, UIController);
